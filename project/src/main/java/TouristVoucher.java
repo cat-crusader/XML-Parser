74b767e7bab5463bf70a71e307105261054b4bb8
Cat Crusader <cat.crusader31@gmail.com>
2021-12-07 15:21:50 +0200

    add java project, start implementing classes

---
+++ project/src/main/java/TouristVoucher.java
@@ -0,0 +1,23 @@
+public class TouristVoucher {
+    int id;
+
+
+
+}
+//<trips>
+//<Тourist_voucher>
+//<id>1</id>
+//<type>excursion</type>
+//<country>Ukraine</country>
+//<duration>7</duration>
+//<transport>helicopter</transport>
+//<hc>
+//<food>BB</food>
+//<stars>5</stars>
+//<aircooller>true</aircooller>
+//<tv>false</tv>
+//</hc>
+//<cost>10000</cost>
+//</Тourist_voucher>
+//
+//</trips>
