6c5d8806aadb77f456809a67b67a96b0212650f1
Cat Crusader <cat.crusader31@gmail.com>
2021-12-29 19:32:21 +0200

    StAX parser refactored,(stable version)

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -14,13 +14,18 @@ public class Main {
 
         public static void main(String[] args) {
 
-                                System.out.print("Validator lol: ");
-                                System.out.println(ValidatorXML.validateAgainstXSD("tourist_voucher.xml", "tourist_voucher.xsd"));
+                System.out.print("Validator lol: ");
+                System.out.println(ValidatorXML.validateAgainstXSD("tourist_voucher.xml", "tourist_voucher.xsd"));
 
-                                TouristVoucherParser pars = new TouristVoucherParser();
-                                System.out.println();
-                                System.out.println("DOM parser");
-                                pars.parse("xml/tourist_vouchers.xml","tourist_voucher.xsd","dom");
+                TouristVoucherParser pars = new TouristVoucherParser();
+                System.out.println();
+                System.out.println("DOM parser");
+                pars.parse("xml/tourist_vouchers.xml","tourist_voucher.xsd","dom");
+
+                System.out.println();
+                System.out.println("StAX parser");
+                pars.parse("xml/tourist_vouchers.xml","tourist_voucher.xsd","stax");
+
 //                                MyDOMParser p = new MyDOMParser( "xml/tourist_vouchers.xml", "tourist_voucher.xsd");
 //                                List<TouristVoucher> touristVouchers = p.parseXML();
 //                                for (TouristVoucher k : touristVouchers){
--- project/src/main/java/parsers/MyStAXParser.java
+++ project/src/main/java/parsers/MyStAXParser.java
@@ -9,104 +9,56 @@ import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyStAXParser extends MyParser {
+public class MyStAXParser implements ParserXML {
 
-    public MyStAXParser(String xml_path, String xsd_path){
-        super(xml_path, xsd_path);
+    private XMLHandler handler;
+
+    MyStAXParser(XMLHandler _handler){
+        handler=_handler;
     }
 
-    public List<TouristVoucher> parseXML() throws IOException {
-        if (!validateXMLByXSD())
-            throw new IOException();
-        List<TouristVoucher> touristVouchers = new ArrayList<>();
-        Integer id = null;
-        String type = null;
-        String country = null;
-        int duration = 1;
-        String transport = null;
-        String hc = null;
-        int cost = 10;
-        String food = null;
-        int stars = 5;
-        boolean aircooler = false;
-        boolean tv = false;
-
-        String taq = "no taq";
-
+    @Override
+    public void parse(String xmlPath) {
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        XMLEventReader reader = null;
         try {
-            XMLInputFactory factory = XMLInputFactory.newInstance();
-            XMLEventReader eventReader =
-                    factory.createXMLEventReader(new FileReader(xml_path));
-
-            while (eventReader.hasNext()) {
-                XMLEvent event = eventReader.nextEvent();
-
-                switch (event.getEventType()) {
-                    case XMLStreamConstants.START_ELEMENT -> {
-                        StartElement startElement = event.asStartElement();
-                        String qName = startElement.getName().getLocalPart();
-                        if (qName.equalsIgnoreCase("id")) {
-                            taq = "id";
-                        } else if (qName.equalsIgnoreCase("type")) {
-                            taq = "type";
-                        } else if (qName.equalsIgnoreCase("country")) {
-                            taq = "country";
-                        } else if (qName.equalsIgnoreCase("duration")) {
-                            taq = "duration";
-                        } else if (qName.equalsIgnoreCase("transport")) {
-                            taq = "transport";
-                        } else if (qName.equalsIgnoreCase("hc")) {
-                            taq = "hc";
-                        } else if (qName.equalsIgnoreCase("cost")) {
-                            taq = "cost";
-                        } else if (qName.equalsIgnoreCase("food")) {
-                            taq = "food";
-                        } else if (qName.equalsIgnoreCase("stars")) {
-                            taq = "stars";
-                        } else if (qName.equalsIgnoreCase("aircooler")) {
-                            taq = "aircooler";
-                        } else if (qName.equalsIgnoreCase("tv")) {
-                            taq = "tv";
-                        } else {
-                            taq = "no taq";
-                        }
-                    }
-                    case XMLStreamConstants.CHARACTERS -> {
-                        Characters characters = event.asCharacters();
-                        switch (taq) {
-                            case "id" -> id = Integer.parseInt(characters.toString());
-                            case "type" -> type = characters.toString();
-                            case "country" -> country = characters.toString();
-                            case "duration" -> duration = Integer.parseInt(characters.toString());
-                            case "transport" -> transport = characters.toString();
-                            case "hc" -> hc = characters.toString();
-                            case "cost" -> cost = Integer.parseInt(characters.toString());
-                            case "food" -> food = characters.toString();
-                            case "stars" -> stars = Integer.parseInt(characters.toString());
-                            case "aircooler" -> aircooler = Boolean.parseBoolean(characters.toString());
-                            case "tv" -> tv = Boolean.parseBoolean(characters.toString());
-                        }
-                        taq = "no taq";
-                    }
-                    case XMLStreamConstants.END_ELEMENT -> {
-                        EndElement endElement = event.asEndElement();
-                        if (endElement.getName().getLocalPart().equalsIgnoreCase("touristvoucher")) {
-                            touristVouchers.add(new TouristVoucher(id, type, country, duration, transport, hc,
-                                    cost, food, stars, aircooler, tv));
-                        }
-                    }
-                }
-            }
+            reader = factory.createXMLEventReader(new FileInputStream(xmlPath));
         } catch (FileNotFoundException | XMLStreamException e) {
             e.printStackTrace();
         }
-        return touristVouchers;
+
+        while (reader.hasNext()) {
+            try {
+                XMLEvent event = reader.nextEvent();
+
+                switch (event.getEventType()) {
+                    case XMLStreamConstants.END_DOCUMENT:
+                        break;
+
+                    case XMLStreamConstants.START_ELEMENT:
+                        StartElement startElement = event.asStartElement();
+                        event = reader.nextEvent();
+                        handler.setTag(startElement.getName().getLocalPart(), event.asCharacters().getData());
+                        break;
+
+                    case XMLStreamConstants.END_ELEMENT:
+                        EndElement endElement = event.asEndElement();
+                        if (endElement.getName().getLocalPart().equalsIgnoreCase(handler.name)) {
+                            handler.endTag(handler.name);
+                        }
+                        break;
+                }
+            } catch (XMLStreamException e) {
+            }
+        }
+        return;
     }
 
 }
